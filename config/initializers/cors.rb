a932df544aab6f0d4537aa813cfb76a994d6e0d4
hannahgmacca <hannahgmacca@gmail.com>
2021-12-08 20:52:41 +1000

    Change CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-   origins 'http://localhost:3000'
-   resource ‘*’,
+   origins 'https://tutal-backend.herokuapp.com/'
+   resource '*',
    headers: :any,
    methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
