90ddf3f4d1b53231cf5ed6a59e3f0fc2aab6a8b8
hannahgmacca <hannahgmacca@gmail.com>
2021-12-09 11:24:28 +1000

    Add index controller to user for debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
       end 
   end 
 
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
   private 
   def user_params 
       params.permit(:username, :email, :password, :password_confirmation)
