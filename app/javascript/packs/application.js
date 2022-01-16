197b359a4001b072bc8ab752f56bb2e0c69a8667
hannahgmacca <hannahgmacca@gmail.com>
2022-01-16 11:46:08 +1100

    Delete and recreate credentientials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,7 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+// import * as ActiveStorage from "@rails/activestorage"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+Rails.start();
+Turbolinks.start();
+// ActiveStorage.start();
